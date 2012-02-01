2f99129c203b860285c517fa390ad18e5d665bda
David Mignot <david.mig@gmail.com>
2012-02-01 21:44:22 +0100

    make it look like a compass extension

---
+++ lib/bootstrap-sass-scss-compass.rb
@@ -0,0 +1,9 @@
+require 'compass'
+Compass::Frameworks.register("bootstrap-sass-scss-compass", :path => File.join(File.dirname(__FILE__), ".."))
+
+module BootstrapSassScssCompass
+  
+  VERSION = "2.0.0"
+  DATE = "2012-02-01"
+
+end
---
+++ templates/project/manifest.rb
@@ -0,0 +1,19 @@
+description 'bootstrap-sass-scss-compass Gem'
+
+# Image Files
+image 'images/glyphicons-halflings.png', :to => 'glyphicons-halflings.png'
+image 'images/glyphicons-halflings-white.png', :to => 'glyphicons-halflings-white.png'
+
+# Javascrips
+javascript 'javascripts/bootstrap-alert.js', :to => 'bootstrap-alert.js'
+javascript 'javascripts/bootstrap-button.js', :to => 'bootstrap-button.js'
+javascript 'javascripts/bootstrap-carousel.js', :to => 'bootstrap-carousel.js'
+javascript 'javascripts/bootstrap-collapse.js', :to => 'bootstrap-collapse.js'
+javascript 'javascripts/bootstrap-dropdown.js', :to => 'bootstrap-dropdown.js'
+javascript 'javascripts/bootstrap-modal.js', :to => 'bootstrap-modal.js'
+javascript 'javascripts/bootstrap-popover.js', :to => 'bootstrap-popover.js'
+javascript 'javascripts/bootstrap-scrollspy.js', :to => 'bootstrap-scrollspy.js'
+javascript 'javascripts/bootstrap-tab.js', :to => 'bootstrap-tab.js'
+javascript 'javascripts/bootstrap-tooltip.js', :to => 'bootstrap-tooltip.js'
+javascript 'javascripts/bootstrap-transition.js', :to => 'bootstrap-transition.js'
+javascript 'javascripts/bootstrap-typeahead.js', :to => 'bootstrap-typeahead.js'
